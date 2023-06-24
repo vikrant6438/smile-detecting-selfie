019d494781d1dca1411cd3b093923639fda94a5c
vikrant singh <69213508+vikrant6438@users.noreply.github.com>
2023-06-24 06:08:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier("dataset/haarcascade_frontalface_default.xml")
+smileCascade = cv2.CascadeClassifier("dataset/haarcascade_smile.xml")
+count = 1
+while True:
+    success,img = video.read()
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(grayImg,1.1,4)
+    
+    keyPressed = cv2.waitKey(1)
+    for x,y,w,h in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0),3)
+        smiles = smileCascade.detectMultiScale(grayImg,1.8,15)
+        for x,y,w,h in smiles:
+            
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(100,100,100),5)
+            path='image'+str(count)+'.jpg'
+            cv2.imwrite(path,img)
+            print("Image "+str(count)+"Saved")
+            count += 1
+                
+    cv2.imshow('live video',img)
+    if(keyPressed & 0xFF==ord('q')):
+        break
+
+video.release()                                  
+cv2.destroyAllWindows() 
